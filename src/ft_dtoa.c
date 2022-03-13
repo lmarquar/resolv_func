c2251996bf10058d043a54906e205949884228c2
Leon Marquardt <lmarquar@c2s10d43.42wolfsburg.de>
2022-03-13 11:14:18 +0100

    added

--- src/ft_dtoa.c
+++ src/ft_dtoa.c
@@ -1,29 +1,29 @@
 #include <unistd.h>
 
-int ft_putlong(long d)
+//not error prone, if values of double overstep limits
+
+int ft_putlong(long ld)
 {
 	char	c;
 
-	if (d < 0)
+	if (ld < 0)
 	{
-		d = d * -1;
+		ld = ld * -1;
 		write(1, "-", 1);
 	}
-	if (d <= 9)
+	if (ld <= 9)
 	{
-		c = d + '0';
+		c = ld + '0';
 		write(1, &c, 1);	
 	}
 	else
 	{
-		c = (d % 10) + '0';
-		ft_putlong(d / 10);
+		c = (ld % (long)10) + '0';
+		ft_putlong(ld / 10);
 		write(1, &c, 1);
 	}
 	return (1);
 }
-
-#include <stdio.h>
 
 int	ft_putdouble(double d)
 {
@@ -37,29 +37,15 @@ int	ft_putdouble(double d)
 	}
 	ld = (long) d;
 	ft_putlong(ld);
-	d = d - (double) ld;
 	i = 0;
-	printf("%lf", d);
 	ld = (long) d;
-	while (((double)ld - d) != 0.0)
-	{
-		printf("\n%d:\t%lf\n\t%lf\n", i, d, (double)ld);
-		printf("%lf", ((double)ld - d));
-		d = d * 10.0;
-		ld = (long) d;
-		i++;
-	}
-	printf("\n%ld\n", ld);
-	if (i == 0)
+	d = d - (double) ld;
+	ld = (long) d;
+	d = d * ((double)(1e15));
+	ld = ld * ((long)(1e15));
+	if ((long)d == 0)
 		return (0);
 	write(1, ".", 1);
-	ft_putlong(ld);
+	ft_putlong((long)d);
 	return (0);
 }
-
-int main()
-{
-	ft_putdouble(42.42);
-	return (0);
-}
-
